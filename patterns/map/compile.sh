5ac590c200b2c08cd11714d17402da3c79ecbbe5
unknown <davidbaicoloradocollege@gmail.com>
2017-10-02 14:23:57 -0600

    OpenMP pin finding 1.0

--- patterns/map/compile.sh
+++ patterns/map/compile.sh
@@ -7,10 +7,10 @@ gcc -o recover_pins_serial serial.c -lcrypto -lm
 gcc -o recover_pins_openmp -fopenmp openmp.c -lcrypto
 
 # Compile the cilkplus version
-gcc -o recover_pins_cilk -fcilkplus cilk.c -lcrypto
+#gcc -o recover_pins_cilk -fcilkplus cilk.c -lcrypto
 
 # Compile the tbb version
-g++ -o recover_pins_tbb tbb.c -ltbb
+#g++ -o recover_pins_tbb tbb.c -ltbb
 
 # Compile the MPI version
-mpicc -o recover_pins_mpi mpi.c
+#mpicc -o recover_pins_mpi mpi.c

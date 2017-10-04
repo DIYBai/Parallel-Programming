584ddf058534ba457129941c328ed84ba3b119c0
unknown <davidbaicoloradocollege@gmail.com>
2017-10-04 15:12:28 -0600

    Adding cilk compilation

--- patterns/stencil/compile2.sh
+++ patterns/stencil/compile2.sh
@@ -2,5 +2,6 @@ srun g++ -o stencil_serial serial.cpp -lopencv_core -lopencv_imgcodecs
 srun g++ -o stencil_openmp -fopenmp openmp.cpp -lopencv_core -lopencv_imgcodecs
 #srun g++ -o stencil_openmp2 -fopenmp openmp2.cpp -lopencv_core -lopencv_imgcodecs
 #srun g++ -o stencil_openmp3 -fopenmp openmp3.cpp -lopencv_core -lopencv_imgcodecs
+srun g++ -o stencil_cilk -fcilkplus cilk.cpp -lopencv_core -lopencv_imgcodecs
 #srun g++ -o stencil_tbb tbb.cpp -ltbb -lopencv_core -lopencv_imgcodecs
 #srun mpic++ -o stencil_mpi mpi.cpp -lopencv_core -lopencv_imgcodecs

f99453f5ac18a2d2bdb4063efba3c1952141fa9d
unknown <davidbaicoloradocollege@gmail.com>
2017-10-09 17:19:59 -0600

    Set-up files for project

---
+++ project/src/inclusion.hpp
@@ -0,0 +1,8 @@
+#include <opencv2/core/Core.hpp>
+#include <opencv2/core/Mat.hpp>
+#include <opencv2/core/MatOfRect.hpp>
+#include <opencv2/core/Point.hpp>
+#include <opencv2/core/Rect.hpp>
+#include <opencv2/core/Scalar.hpp>
+#include <opencv2/highgui.Highgui.hpp>
+#include <opencv2/objdetect.CascadeClassifier.hpp>

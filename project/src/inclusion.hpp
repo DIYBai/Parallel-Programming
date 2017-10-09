a5726cb486a5cb93b6545fb375ce26ffaee84c53
unknown <davidbaicoloradocollege@gmail.com>
2017-10-09 17:31:04 -0600

    Java opencv modules have different names

--- project/src/inclusion.hpp
+++ project/src/inclusion.hpp
@@ -1,8 +1,9 @@
-#include <opencv2/core/Core.hpp>
-#include <opencv2/core/Mat.hpp>
-#include <opencv2/core/MatOfRect.hpp>
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <opencv2/core/core.hpp>
+#include <opencv2/core/mat.hpp>
 #include <opencv2/core/Point.hpp>
 #include <opencv2/core/Rect.hpp>
 #include <opencv2/core/Scalar.hpp>
 #include <opencv2/highgui.Highgui.hpp>
-#include <opencv2/objdetect.CascadeClassifier.hpp>
+#include <opencv2/objdetect.objdetect.hpp>

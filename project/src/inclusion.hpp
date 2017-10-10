e3440553cb69b6f4e6f3a54e1c7e1ecf1e9e14b9
unknown <davidbaicoloradocollege@gmail.com>
2017-10-09 18:15:26 -0600

    Add some comments

--- project/src/inclusion.hpp
+++ project/src/inclusion.hpp
@@ -2,8 +2,8 @@
 #include <cmath>
 #include <opencv2/core/core.hpp>
 #include <opencv2/core/mat.hpp>
-#include <opencv2/core/Point.hpp>
-#include <opencv2/core/Rect.hpp>
-#include <opencv2/core/Scalar.hpp>
-#include <opencv2/highgui/Highgui.hpp>
+#include <opencv2/core/Point.hpp> //necessary?
+#include <opencv2/core/Rect.hpp> //necessary?
+#include <opencv2/core/Scalar.hpp> //can't find
+#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/objdetect/objdetect.hpp>

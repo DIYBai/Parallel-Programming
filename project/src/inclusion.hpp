9c427a4eceb9fc98b53000a50e4ae2de6dc8782f
unknown <davidbaicoloradocollege@gmail.com>
2017-10-11 09:31:19 -0600

    Perhaps order of includes matters

--- project/src/inclusion.hpp
+++ project/src/inclusion.hpp
@@ -3,6 +3,6 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/core/mat.hpp>
 // #include <opencv2/core/Scalar.hpp> //can't find
-#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>

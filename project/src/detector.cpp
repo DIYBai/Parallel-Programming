99e1ed1af7acc4e3930c6d07d3b8091981bd7970
unknown <davidbaicoloradocollege@gmail.com>
2017-10-12 02:18:53 -0600

    Hopefully completely blurring one face.

--- project/src/detector.cpp
+++ project/src/detector.cpp
@@ -4,28 +4,81 @@
 #include "inclusion.hpp"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <cmath>
 
 using namespace cv;
 using namespace std; //unsure if this is necessary/desireable
 
-//TODO:
-// int detectFace(pixel * frame_data, int frame_num, int *coords){ //return bool
-//   //maybe do this initilization for-loop outside/before function call
-//   for(int i = 0; i < 4; i++){
-//     coords[i] = -1;
-//   }
-//
-//   if(FIND_FACE){
-//       coords[0] = x1_VAR;
-//       coords[1] = y1_VAR;
-//       coords[2] = x2_VAR;
-//       coords[3] = y2_VAR;
-//       return 1;
-//   }
-//   return 0;
-// }
+struct pixel {
+    double red;
+    double green;
+    double blue;
+    double intensity;
 
+    pixel(double r, double g, double b) : red(r), green(g), blue(b) {};
+};
 
+void gaussian_kernel(const int size, const double stddev, double * const kernel) {
+    // values needed for the curve
+    const double denom = 2.0 * stddev * stddev;
+    const double g_denom = M_PI * denom;
+    const double g_denom_recip = (1.0/g_denom);
+    // accumulator so that values can be normalized to 1
+    double sum = 0.0;
+
+    // Build the template
+    for(int i = 0; i < size; ++i) {
+        for(int j = 0; j < size; ++j) {
+            const double row_dist = i - (size/2);
+            const double col_dist = j - (size/2);
+            const double dist_sq = (row_dist * row_dist) + (col_dist * col_dist);
+            const double value = g_denom_recip * exp((-dist_sq)/denom);
+            kernel[i + (j*size)] = value;
+            sum += value;
+        }
+    }
+
+    // Normalize
+    const double recip_sum = 1.0 / sum;
+    for(int i = 0; i < size; ++i) {
+        for(int j = 0; j < size; ++j) {
+            kernel[i + (j*size)] *= recip_sum;
+        }
+    }
+}
+
+void apply_blur(int radius, const double stddev, const int x1, const int y1, const int x2, const int y2, const int rows, const int cols, pixel * const in, pixel * const out) {
+    //make sure 0 <= x1 < x2 <= cols and 0 <= y1 < y2 <= rows ??
+    const int dim = radius*2+1;
+    double kernel[dim*dim];
+    gaussian_kernel(dim, stddev, kernel);
+
+    // #pragma omp parallel for
+    for(int i = y1; i < y2; ++i) {
+        for(int j = x1; j < x2; ++j) {
+            const int out_offset = i + (j*rows);
+            //NOTE: small change from adapted code
+            //TODO: Mostly works, but check edge cases (see example in misc)
+            out[out_offset].red   = 0;
+            out[out_offset].green = 0;
+            out[out_offset].blue  = 0;
+            for(int x = i - radius, kx = 0; x <= i + radius; ++x, ++kx) {
+                for(int y = j - radius, ky = 0; y <= j + radius; ++y, ++ky) {
+                    //TODO: consider the edge cases instead of treating them as 0s (by not adding)
+                    if(x >= 0 && x < rows && y >= 0 && y < cols) {
+                        // Acculate intensities in the output pixel
+                        const int in_offset = x + (y*rows);
+                        const int k_offset = kx + (ky*dim);
+                        out[out_offset].red   += kernel[k_offset] * in[in_offset].red;
+                        out[out_offset].green += kernel[k_offset] * in[in_offset].green;
+                        out[out_offset].blue  += kernel[k_offset] * in[in_offset].blue;
+                    }
+                }
+            }
+        }
+    }
+}
 
 int main(int argc, char **argv){
     if(argc != 2) {
@@ -33,46 +86,54 @@ int main(int argc, char **argv){
         return 1;
     }
 
-    // Rect test = Rect(1, 1, 5, 5);
-    // printf("Rect test: %d %d %d %d\n", test.x, test.y, test.width, test.height);
-    // Rect test2 = Rect(1, 2, 3, 4);
-    // vector <Rect> testVec;
-    // testVec.push_back(test);
-    // testVec.push_back(test2);
-    //
-    // for ( vector <Rect>::iterator rect_iter = testVec.begin(); rect_iter != testVec.end(); ++rect_iter) {
-    //     // Core.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0));
-    //     printf("Face found at (%d)\n", rect_iter->x);//, rect_iter.y, rect_iter.x + rect_iter.width, rect_iter.y + rect_iter.height);
-    // }
-    //printf("Running FaceDetector\n");
-
-    // JC: CascadeClassifier faceDetector = new CascadeClassifier(FaceDetector.class.getResource("haarcascade_frontalface_alt.xml").getPath());
     // Obtained from: https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_frontalface_alt.xml
-    CascadeClassifier faceDetector = CascadeClassifier("haarcascade_frontalface_alt.xml"); // = malloc(sizeof(CascadeClassifier));
+    CascadeClassifier faceDetector = CascadeClassifier("haarcascade_frontalface_alt.xml");
     // faceDetector.load("./haarcascade_face.xml");
 
-    // JC: Mat image = Highgui.imread(FaceDetector.class.getResource("shekhar.JPG").getPath());
-    // Mat image = imread("test1.jpg");
-    Mat image = imread(argv[1]);
+    //put for loop starting here to read all pictures in folder
+    Mat image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
     if(image.empty()){
-      printf("Empty/bad file\n");
+      printf("Empty or bad file\n");
       return -1;
     }
 
-    // JC: MatOfRect faceDetections = new MatOfRect();
+    const int rows = image.rows;
+    const int cols = image.cols;
+    pixel * imagePixels = (pixel *) malloc(rows * cols * sizeof(pixel));
+    for(int i = 0; i < rows; ++i) {
+        for(int j = 0; j < cols; ++j) {
+            Vec3b p = image.at<Vec3b>(i, j);
+            imagePixels[i + (j*rows)] = pixel(p[0]/255.0,p[1]/255.0,p[2]/255.0);
+        }
+    }
+
+    //create output pixels
+    pixel * outPixels = (pixel *) malloc(rows * cols * sizeof(pixel));
+    memcpy(outPixels, inPixels, sizeof(pixel)*rows*cols);
+
     // https://stackoverflow.com/questions/15893591/confusion-between-opencv4android-and-c-data-types
     vector <Rect> faceDetections;
     faceDetector.detectMultiScale(image, faceDetections);
 
-    // JC: System.out.println(String.format("Detected %s faces", faceDetections.toArray().length));
-    printf( "Detected %d face(s)\n", faceDetections.size() );
-
-    for ( vector <Rect>::iterator rect_iter = faceDetections.begin(); rect_iter != faceDetections.end(); ++rect_iter) {
-        printf("Face found at (%d, %d), (%d, %d)\n", rect_iter->x, rect_iter->y, rect_iter->x + rect_iter->width, rect_iter->y + rect_iter->height);
+    if(faceDetections.size() > 0 ){ //might not need this statement
+        for ( vector <Rect>::iterator rect_iter = faceDetections.begin(); rect_iter != faceDetections.end(); ++rect_iter) {
+            apply_blur(3, 32.0, rect_iter->x, rect_iter->y, rect_iter->x + rect_iter->width, rect_iter->y + rect_iter->height, rows, cols, inPixels, outPixels);
+        }
     }
 
-    return 1;
-    // string filename = "output.png";
-    // printf("Writing %s", filename);
-    // imwrite(filename, image);
+    Mat dest(rows, cols, CV_8UC3);
+    for(int i = 0; i < rows; ++i) {
+        for(int j = 0; j < cols; ++j) {
+            const size_t offset = i + (j*rows);
+            dest.at<Vec3b>(i, j) = Vec3b(floor(outPixels[offset].red * 255.0),
+                                         floor(outPixels[offset].green * 255.0),
+                                         floor(outPixels[offset].blue * 255.0));
+        }
+    }
+    imwrite("TEMP.jpg", dest);
+
+    free(imagePixels);
+    free(outPixels);
+
+    return 0;
 }

44b488cd51ce24711fad1f5dd9cca8e280513ddd
unknown <davidbaicoloradocollege@gmail.com>
2017-10-12 21:50:04 -0600

    Apparently node0 is the worst.

--- project/src/comp2.sh
+++ project/src/comp2.sh
@@ -1,1 +1,1 @@
-srun g++ -o detect_ser detect_ser.cpp -lopencv_core -lopencv_imgcodecs -lopencv_objdetect
+srun -x node0 g++ -o detect_ser detect_ser.cpp -lopencv_core -lopencv_imgcodecs -lopencv_objdetect

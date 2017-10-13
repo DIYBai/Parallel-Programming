2bab33a5af209f453012b72e1ce001ac8d1acccb
unknown <davidbaicoloradocollege@gmail.com>
2017-10-12 22:02:37 -0600

    Adding node0 back to the mix

--- project/src/comp2.sh
+++ project/src/comp2.sh
@@ -1,1 +1,1 @@
-srun -x node0 g++ -o detect_ser detect_ser.cpp -lopencv_core -lopencv_imgcodecs -lopencv_objdetect
+srun g++ -o detect_ser detect_ser.cpp -lopencv_core -lopencv_imgcodecs -lopencv_objdetect

c389166b2cd186304a6c0d78892e32f6cfa099a9
unknown <davidbaicoloradocollege@gmail.com>
2017-10-14 15:32:04 -0600

    JPG -> PNG

--- project/src/do.sh
+++ project/src/do.sh
@@ -1,22 +1,22 @@
 cd input
-rm *.jpg
+rm *.png
 cd ..
 bash get_frames.sh
 
 bash comp.sh
 
 cd serial_out
-rm *.jpg
+rm *.png
 cd ..
 ./detect_ser input serial_out #> ser_fnames.txt
 
 cd omp_out
-rm *.jpg
+rm *.png
 cd ..
 ./detect_omp input omp_out #> omp_fnames.txt
 
 cd cilk_out
-rm *.jpg
+rm *.png
 cd ..
 ./detect_cilk input cilk_out
 

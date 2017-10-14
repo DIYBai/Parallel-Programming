13c5d66f4970e957203c27613d8d71470b2dcb75
unknown <davidbaicoloradocollege@gmail.com>
2017-10-14 15:30:39 -0600

    Using .pngs instead of .jpgs

--- project/src/get_frames.sh
+++ project/src/get_frames.sh
@@ -1,2 +1,2 @@
-ffmpeg -i input/input.mp4 -vf fps=12 input/in%06d.jpg
+ffmpeg -i input/input.mp4 -vf fps=12 input/in%06d.png
 #does it have to be flv??
--- project/src/write_to_video.sh
+++ project/src/write_to_video.sh
@@ -1,4 +1,4 @@
-ffmpeg -r 12 -f image2 -s 1920x1080 -i input/in%06d.jpg -vcodec mpeg4 -pix_fmt yuv420p serial_out/OG_output.mp4
-ffmpeg -r 12 -f image2 -s 1920x1080 -i serial_out/out_in%06d.jpg -vcodec mpeg4 -pix_fmt yuv420p serial_out/ser_output.mp4
-ffmpeg -r 12 -f image2 -s 1920x1080 -i omp_out/out_in%06d.jpg -vcodec mpeg4 -pix_fmt yuv420p omp_out/ser_output.mp4
-# ffmpeg -r 24 -f image2 -s 1920x1080 -i cilk_out/out_in%06d.jpg -vcodec mpeg4 -pix_fmt yuv420p cilk_out/ser_output.mp4
+ffmpeg -r 12 -f image2 -s 1920x1080 -i input/in%06d.png -vcodec mpeg4 -pix_fmt yuv420p serial_out/OG_output.mp4
+ffmpeg -r 12 -f image2 -s 1920x1080 -i serial_out/out_in%06d.png -vcodec mpeg4 -pix_fmt yuv420p serial_out/ser_output.mp4
+ffmpeg -r 12 -f image2 -s 1920x1080 -i omp_out/out_in%06d.png -vcodec mpeg4 -pix_fmt yuv420p omp_out/omp_output.mp4
+ffmpeg -r 12 -f image2 -s 1920x1080 -i cilk_out/out_in%06d.png -vcodec mpeg4 -pix_fmt yuv420p cilk_out/cilk_output.mp4

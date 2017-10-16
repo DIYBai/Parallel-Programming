a865d625c268f58a59e31037b6418963d69aa6a0
unknown <davidbaicoloradocollege@gmail.com>
2017-10-16 00:43:12 -0600

    Create output videos

--- project/src/scripts/do.sh
+++ project/src/scripts/do.sh
@@ -1,22 +1,28 @@
-bash scripts/get_frames.sh
+# bash scripts/get_frames.sh
 
 # bash scripts/comp.sh
 
-bash scripts/time.sh
+# bash scripts/time.sh
 
 # cd 12serial_out
 # rm *.png
 # cd ..
+./bin/detect_ser 2input 2serial_out #> ser_fnames.txt
 # ./bin/detect_ser 12input 12serial_out #> ser_fnames.txt
-#
+./bin/detect_ser 24input 24serial_out #> ser_fnames.txt
+
 # cd 12omp_out
 # rm *.png
 # cd ..
-# ./bin/detect_omp 12input 12omp_out #> omp_fnames.txt
-#
+./bin/detect_omp 2input 2omp_out #> omp_fnames.txt
+# ./bin/detect_omp 12input 12omp_out
+./bin/detect_omp 24input 24omp_out
+
 # cd 12cilk_out
 # rm *.png
 # cd ..
+./bin/detect_cilk 2input 2cilk_out
 # ./bin/detect_cilk 12input 12cilk_out
+./bin/detect_cilk 24input 24cilk_out
 
-# bash write_to_video.sh
+bash scripts/write_to_video.sh
--- project/src/scripts/write_to_video.sh
+++ project/src/scripts/write_to_video.sh
@@ -1,24 +1,24 @@
-cd 12serial_out #2serial_out or 12serial_out or 24serial_out
-rm *.mp4
-cd ..
-# ffmpeg -r 2 -f image2 -s 1920x1080 -i 2input/in%06d.png -vcodec mpeg4 -pix_fmt yuv420p 2serial_out/2OG_output.mp4
-# ffmpeg -r 2 -f image2 -s 1920x1080 -i 2serial_out/out_in%06d.png -vcodec mpeg4 -pix_fmt yuv420p 2serial_out/2ser_output.mp4
+# cd 12serial_out #2serial_out or 12serial_out or 24serial_out
+# rm *.mp4
+# cd ..
+ffmpeg -r 2 -f image2 -s 1920x1080 -i 2input/in%06d.png -vcodec mpeg4 -pix_fmt yuv420p 2serial_out/2OG_output.mp4
+ffmpeg -r 2 -f image2 -s 1920x1080 -i 2serial_out/out_in%06d.png -vcodec mpeg4 -pix_fmt yuv420p 2serial_out/2ser_output.mp4
 ffmpeg -r 12 -f image2 -s 1920x1080 -i 12input/in%06d.png -vcodec mpeg4 -pix_fmt yuv420p 12serial_out/12OG_output.mp4
 ffmpeg -r 12 -f image2 -s 1920x1080 -i 12serial_out/out_in%06d.png -vcodec mpeg4 -pix_fmt yuv420p 12serial_out/12ser_output.mp4
-# ffmpeg -r 24 -f image2 -s 1920x1080 -i 24input/in%06d.png -vcodec mpeg4 -pix_fmt yuv420p 24serial_out/24OG_output.mp4
-# ffmpeg -r 24 -f image2 -s 1920x1080 -i 24serial_out/out_in%06d.png -vcodec mpeg4 -pix_fmt yuv420p 24serial_out/24ser_output.mp4
+ffmpeg -r 24 -f image2 -s 1920x1080 -i 24input/in%06d.png -vcodec mpeg4 -pix_fmt yuv420p 24serial_out/24OG_output.mp4
+ffmpeg -r 24 -f image2 -s 1920x1080 -i 24serial_out/out_in%06d.png -vcodec mpeg4 -pix_fmt yuv420p 24serial_out/24ser_output.mp4
 
 
-cd 12omp_out
-rm *.mp4
-cd ..
-# ffmpeg -r 2 -f image2 -s 1920x1080 -i 2omp_out/out_in%06d.png -vcodec mpeg4 -pix_fmt yuv420p 2omp_out/2omp_output.mp4
+# cd 12omp_out
+# rm *.mp4
+# cd ..
+ffmpeg -r 2 -f image2 -s 1920x1080 -i 2omp_out/out_in%06d.png -vcodec mpeg4 -pix_fmt yuv420p 2omp_out/2omp_output.mp4
 ffmpeg -r 12 -f image2 -s 1920x1080 -i 12omp_out/out_in%06d.png -vcodec mpeg4 -pix_fmt yuv420p 12omp_out/12omp_output.mp4
-# ffmpeg -r 24 -f image2 -s 1920x1080 -i 24omp_out/out_in%06d.png -vcodec mpeg4 -pix_fmt yuv420p 24omp_out/24omp_output.mp4
+ffmpeg -r 24 -f image2 -s 1920x1080 -i 24omp_out/out_in%06d.png -vcodec mpeg4 -pix_fmt yuv420p 24omp_out/24omp_output.mp4
 
-cd 12cilk_out
-rm *.mp4
-cd ..
-# ffmpeg -r 2 -f image2 -s 1920x1080 -i 2cilk_out/out_in%06d.png -vcodec mpeg4 -pix_fmt yuv420p 2cilk_out/2cilk_output.mp4
+# cd 12cilk_out
+# rm *.mp4
+# cd ..
+ffmpeg -r 2 -f image2 -s 1920x1080 -i 2cilk_out/out_in%06d.png -vcodec mpeg4 -pix_fmt yuv420p 2cilk_out/2cilk_output.mp4
 ffmpeg -r 12 -f image2 -s 1920x1080 -i 12cilk_out/out_in%06d.png -vcodec mpeg4 -pix_fmt yuv420p 12cilk_out/12cilk_output.mp4
-# ffmpeg -r 24 -f image2 -s 1920x1080 -i 24cilk_out/out_in%06d.png -vcodec mpeg4 -pix_fmt yuv420p 24cilk_out/24cilk_output.mp4
+ffmpeg -r 24 -f image2 -s 1920x1080 -i 24cilk_out/out_in%06d.png -vcodec mpeg4 -pix_fmt yuv420p 24cilk_out/24cilk_output.mp4

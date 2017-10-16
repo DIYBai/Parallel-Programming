6ec05f41bbe6503fede0e8f232d5124b458b53e5
unknown <davidbaicoloradocollege@gmail.com>
2017-10-16 00:22:12 -0600

    Typo

--- project/src/scripts/comp2.sh
+++ project/src/scripts/comp2.sh
@@ -1,3 +1,3 @@
-srun g++ -o ../bin/detect_ser  detect_ser.cpp  -lopencv_core -lopencv_imgcodecs -lopencv_objdetect
-srun g++ -o ../bin/detect_omp  detect_omp.cpp  -lopencv_core -lopencv_imgcodecs -lopencv_objdetect -fopenmp
-srun g++ -o ../bin/detect_cilk detect_cilk.cpp -lopencv_core -lopencv_imgcodecs -lopencv_objdetect -fcilkplus
+srun g++ -o bin/detect_ser  detect_ser.cpp  -lopencv_core -lopencv_imgcodecs -lopencv_objdetect
+srun g++ -o bin/detect_omp  detect_omp.cpp  -lopencv_core -lopencv_imgcodecs -lopencv_objdetect -fopenmp
+srun g++ -o bin/detect_cilk detect_cilk.cpp -lopencv_core -lopencv_imgcodecs -lopencv_objdetect -fcilkplus

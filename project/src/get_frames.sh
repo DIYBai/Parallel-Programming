80cddeaf1025f93c2f03ee6352ffa0c390b9392d
unknown <davidbaicoloradocollege@gmail.com>
2017-10-15 19:27:11 -0600

    Attempt to do 24fps again

--- project/src/get_frames.sh
+++ project/src/get_frames.sh
@@ -2,5 +2,5 @@ cd input
 rm *.png
 cd ..
 
-#ffmpeg -i input/input.mp4 -vf fps=12 input/in%06d.png
-ffmpeg -i input/input.mp4 -vf fps=2 input/in%06d.png
+ffmpeg -i input/input.mp4 -vf fps=24 input/in%06d.png
+#ffmpeg -i input/input.mp4 -vf fps=2 input/in%06d.png

019c57b0831a53c0ef737e5597b803b9d306cde5
unknown <davidbaicoloradocollege@gmail.com>
2017-10-15 23:23:14 -0600

    Timing 12 fps with do.sh

--- project/src/do.sh
+++ project/src/do.sh
@@ -2,19 +2,20 @@ bash get_frames.sh
 
 bash comp.sh
 
-cd serial_out
-rm *.png
-cd ..
-./detect_ser input serial_out #> ser_fnames.txt
+bash time.sh
+# cd serial_out
+# rm *.png
+# cd ..
+# ./detect_ser input serial_out #> ser_fnames.txt
+#
+# cd omp_out
+# rm *.png
+# cd ..
+# ./detect_omp input omp_out #> omp_fnames.txt
+#
+# cd cilk_out
+# rm *.png
+# cd ..
+# ./detect_cilk input cilk_out
 
-cd omp_out
-rm *.png
-cd ..
-./detect_omp input omp_out #> omp_fnames.txt
-
-cd cilk_out
-rm *.png
-cd ..
-./detect_cilk input cilk_out
-
-bash write_to_video.sh > misc.txt
+# bash write_to_video.sh > misc.txt

a556e0d03487c78836a11ec6ac48effe66b402b9
unknown <davidbaicoloradocollege@gmail.com>
2017-10-16 22:08:46 -0600

    More instructions

--- project/src/scripts/get_frames.sh
+++ project/src/scripts/get_frames.sh
@@ -1,7 +1,7 @@
-# cd input #2input, 12input, or 24input
-# rm *.png
-# cd ..
+cd 12input #2input, 12input, or 24input
+rm *.png
+cd ..
 
-ffmpeg -i input/input.mp4 -vf fps=2 2input/in%06d.png
+# ffmpeg -i input/input.mp4 -vf fps=2 2input/in%06d.png
 ffmpeg -i input/input.mp4 -vf fps=12 12input/in%06d.png
-ffmpeg -i input/input.mp4 -vf fps=24 24input/in%06d.png
+# ffmpeg -i input/input.mp4 -vf fps=24 24input/in%06d.png

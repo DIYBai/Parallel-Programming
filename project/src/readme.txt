378fc17dd52b7e92214660b110e40e67c768d9fa
unknown <davidbaicoloradocollege@gmail.com>
2017-10-16 22:08:09 -0600

    Finalizing instructions

---
+++ project/src/readme.txt
@@ -0,0 +1,1 @@
+From this directory, just type "bash run.sh" to run the program.
---
+++ project/src/run.sh
@@ -0,0 +1,28 @@
+module load slurm opencv-3.3.0 gcc-7.2.0 ffmpeg-3.3.4
+mkdir 12input bin 12serial_out 12omp_out 12cilk_out video_out
+
+bash scripts/get_frames.sh
+bash scripts/comp.sh
+
+cd 12serial_out
+rm *.png
+cd ..
+# ./bin/detect_ser 2input 2serial_out #> ser_fnames.txt
+./bin/detect_ser 12input 12serial_out #> ser_fnames.txt
+# ./bin/detect_ser 24input 24serial_out #> ser_fnames.txt
+
+cd 12omp_out
+rm *.png
+cd ..
+# ./bin/detect_omp 2input 2omp_out #> omp_fnames.txt
+./bin/detect_omp 12input 12omp_out
+# ./bin/detect_omp 24input 24omp_out
+
+cd 12cilk_out
+rm *.png
+cd ..
+# ./bin/detect_cilk 2input 2cilk_out
+./bin/detect_cilk 12input 12cilk_out
+# ./bin/detect_cilk 24input 24cilk_out
+
+bash scripts/write_to_video.sh
